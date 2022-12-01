5910c01bccadf3915466632e331e1aa73269d99c
Timothy Vermeiren <timothy.vermeiren@biztory.be>
2022-12-01 06:54:45 +0000

    Formatting

--- webfont.js
+++ webfont.js
@@ -1,17 +1,16 @@
 function loadFont(fontfamily){   
     WebFont.load({
-	google: {
+        google: {
             families: [ fontfamily ]
         },
         context: window.parent
     });
     // Self
     WebFont.load({
-	google: {
-	    families: [ fontfamily ]
-	}
+        google: {
+            families: [ fontfamily ]
+        }
     });
-    
     return true;
 }
 
@@ -19,26 +18,23 @@ function applyFont(fontfamily, classname){
     var x = document.getElementsByClassName(classname);
     var i;
     for (i = 0; i < x.length; i++) {
-      x[i].style.fontFamily = fontfamily;
-      }
+        x[i].style.fontFamily = fontfamily;
+    }
 }
 
 // https://davidwalsh.name/query-string-javascript
 function getUrlParameter(name) {
     name = name.replace(/[\[]/, '\\[').replace(/[\]]/, '\\]');
-        var regex = new RegExp('[\\?&]' + name + '=([^&#]*)');
-            var results = regex.exec(location.search);
-                return results === null ? '' : decodeURIComponent(results[1].replace(/\+/g, ' '));
-                };
+    var regex = new RegExp('[\\?&]' + name + '=([^&#]*)');
+    var results = regex.exec(location.search);
+    return results === null ? '' : decodeURIComponent(results[1].replace(/\+/g, ' '));
+};
                 
 document.addEventListener('DOMContentLoaded', function() {
-    if (loadFont(getUrlParameter('font')))
-    {
-	console.log("Custom font loaded successfully.");
-    } else
-    {
-	console.log("Custom font did not load.");
+    if (loadFont(getUrlParameter('font'))) {
+	    console.log("Custom font loaded successfully.");
+    } else {
+        console.log("Custom font did not load.");
     }
-    
     applyFont(getUrlParameter('font'), 'customfont');
 });
